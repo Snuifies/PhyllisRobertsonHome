4468bf7efe990c9cfbe66b2fd0028993a6a2c58d
Snuifies <MariusSnyman@protonmail.com>
2022-06-14 18:10:15 +0200

    Revert "Refactored to New Spring Security Models"
    This reverts commit e590f5fff0a0cf264391b56ce09450aafec89351.

--- src/main/java/za/org/phyllis/robertson/home/security/config/SecurityConfiguration.java
+++ src/main/java/za/org/phyllis/robertson/home/security/config/SecurityConfiguration.java
@@ -2,16 +2,22 @@ package za.org.phyllis.robertson.home.security.config;
 
 import javax.annotation.Resource;
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import za.org.phyllis.robertson.home.security.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -65,6 +71,7 @@ public class SecurityConfiguration {
                 .deleteCookies("JSESSIONID")
                 .and().csrf().disable()
                 .authenticationProvider(authenticationProvider()) 
+//                                .authenticationManager(authenticationManager);
                 ;
             return http.build();
         } catch (Exception ex) {
