1ac117ef78cdc3ee3fddf671ed40a5b685598d7f
Snuifies <MariusSnyman@protonmail.com>
2022-07-14 20:11:34 +0200

    Initial Create

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'frontend';
+
+  constructor(private primengComcfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.primengComcfig.ripple = true;
+  }
 }
